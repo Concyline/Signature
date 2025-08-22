9eb674b75123b5b907f42dc440a0920cbe2be3ab
AUREO\concy <concyinfo@gmail.com>
2025-08-22 10:39:19 -0300

    modernizacao

--- app/src/main/java/br/com/signature/MainActivity.java
+++ app/src/main/java/br/com/signature/MainActivity.java
@@ -1,14 +1,68 @@
 package br.com.signature;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SignaturePad mSignaturePad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        mSignaturePad = findViewById(R.id.signature_pad);
+        mSignaturePad.setPenColor(Color.BLACK);
+
+        mSignaturePad.setOnSignedListener(new SignaturePad.OnSignedListener() {
+            @Override
+            public void onStartSigning() {
+                System.out.println("OnStartSigning");
+            }
+
+            @Override
+            public void onSigned() {
+                System.out.println("onSigned");
+            }
+
+            @Override
+            public void onClear() {
+                System.out.println("onClear");
+            }
+        });
+
+        findViewById(R.id.clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSignaturePad.clear();
+            }
+        });
+
+
+        findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bitmap signatureBitmap = mSignaturePad.getSignatureBitmap();
+
+                ImageView imageView = findViewById(R.id.imageView);
+
+                imageView.setImageBitmap(signatureBitmap);
+
+                int x = 0;
+            }
+        });
+
     }
 }
